e0d7e42bc442b9bbb41dcaa48eef12e7f2a15b4a
FRANCIELY JESUS <franciely.jesus@senacsp.edu.br>
2023-05-27 10:32:35 -0300

    my files

---
+++ Aula-1/Ola-mundo.js
@@ -0,0 +1,23 @@
+//alert('Este alerta esta vindo de outro arquivo')
+
+ //Tipo de variaveis
+mensagem = "essa mensagem é Global" // string sempre texto
+num_int = 32 //numero inteiro 
+num_float = 1.3 //numero com virgula
+Bool = true
+array = ["Frann", 33, 1.62, true]
+
+
+nome = "Frann_Jesus"
+idade = 33
+altura = 1.62
+maior_idade = true
+cidade = "Sumaré"
+num_casa = 30
+
+
+meus_dados = [nome, idade, altura, maior_idade, cidade, num_casa]
+
+
+
+console.log("uma mensagem no console");
