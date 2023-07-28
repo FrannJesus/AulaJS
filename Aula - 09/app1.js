01a097cb74e8f2b863fdbb65cb2931f388c047d4
FrannJesus <134159403+FrannJesus@users.noreply.github.com>
2023-07-28 09:22:49 -0300

    Update app1.js

--- Aula - 09/app1.js
+++ Aula - 09/app1.js
@@ -1,15 +1,18 @@
 const VerificarItem = document.getElementById('numeroInput');
 console.log(VerificarItem.textContent);
 
+ 
+
 const buttonElement = document.getElementById('checkButton');
 buttonElement.addEventListener('click', function() {
     
-    if (VerificarItem.value < 8) {
+    if (VerificarItem.value =='' ) {
+       VerificarItem.classList(alert('digitar o numero é obrigatório!!!'))
+       alert('digitar o numero é obrigadtório!!!')
+    
+    }else if (VerificarItem.value < 8 ) {
         VerificarItem.classList.add('button-red');
         VerificarItem.classList.remove('button-green');
-    
-    }else if (VerificarItem.value = '') {
-        alert('Digite um numero!!!')
         
     } else {
         
